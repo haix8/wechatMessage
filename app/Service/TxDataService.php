ee8c6ddd91c73e23202876ad768c39ac7d726b2a
K <bbbyer@163.com>
2023-12-13 14:21:02 +0800

    key

--- app/Service/TxDataService.php
+++ app/Service/TxDataService.php
@@ -27,7 +27,7 @@ class TxDataService
             $client = new Client();
             $response = $client->get($uri, [
                 'query' => [
-                    'key' => '58fee8fd45acfe90ff02ed11b6ae4bd9',//参数
+                    'key' => env('tianxing_key'),//参数
                 ]
             ]);
 
@@ -52,7 +52,7 @@ class TxDataService
             $client = new Client();
             $response = $client->get($uri, [
                 'query' => [
-                    'key' => '58fee8fd45acfe90ff02ed11b6ae4bd9',//参数
+                    'key' => env('tianxing_key'),//参数
                 ]
             ]);
 
@@ -72,15 +72,59 @@ class TxDataService
             $client = new Client();
             $response = $client->get($uri, [
                 'query' => [
-                    'key' => '58fee8fd45acfe90ff02ed11b6ae4bd9',//参数
+                    'key' => env('tianxing_key'),//参数
                     'city' => '郑州',
                     'type' => '1'
                 ]
             ]);
-
             return json_decode($response->getBody()->getContents())->result;
         } catch (\Exception $exception) {
             return '';
         }
+
+        /**
+
+        {
+            "code": 200,
+            "msg": "success",
+            "result": {
+                "date": "2023-12-13",
+                "week": "星期三",
+                "province": "河南",
+                "area": "郑州",
+                "areaid": "101180101",
+                "weather": "阴",
+                "weatherimg": "yin.png",
+                "weathercode": "yin",
+                "real": "-0.5℃",
+                "lowest": "-1℃",
+                "highest": "0℃",
+                "wind": "东北风",
+                "windspeed": "11",
+                "windsc": "2级",
+                "sunrise": "07:23",
+                "sunset": "17:15",
+                "moonrise": "",
+                "moondown": "",
+                "pcpn": "0",
+                "uv_index": "0",
+                "aqi": "104",
+                "quality": "轻度",
+                "vis": "1",
+                "humidity": "96",
+                "alarmlist": [
+                    {
+                    "province": "河南省",
+                    "city": "",
+                    "level": "黄色",
+                    "type": "道路结冰",
+                    "content": "河南省气象台2023年12月12日16时00分继续发布道路结冰黄色预警：预计12月12日16时到13日16时，西部、北中部有雨夹雪或中雪，局部大雪或暴雪，部分县市伴有冻雨；其他县市有小到中雨。全省大部县市路表温度低于0℃，将出现对交通有较大影响的道路结冰，其中黄河以北及三门峡、洛阳、郑州、开封、许昌北部、平顶山北部影响较严重。请注意防范。\n防御指南：\n1.交通运输、公安等部门应按照职责做好道路结冰应对准备工作；\n2.驾驶人员应当注意路况，安全行驶；\n3.行人减少外出，必要外出时尽量少骑自行车，注意防滑。（预警信息来源：国家预警信息发布中心）",
+                    "time": "2023-12-12 16:00:00"
+                    }
+                ],
+                "tips": "天气寒冷，冬季着装：棉衣、羽绒服、冬大衣、皮夹克加羊毛衫、厚呢外套、呢帽、手套等；年老体弱者尽量少外出。空气质量较差，敏感人群建议减少体力消耗类户外活动。"
+            }
+        }
+         */
     }
 }
--- app/Service/WechatService.php
+++ app/Service/WechatService.php
@@ -21,8 +21,8 @@ class WechatService
     {
 
         $config = [
-            'app_id' => 'wx894f3363702bfb3a',
-            'secret' => 'd88e0bbea3195c8660240b015496e207',
+            'app_id' => env('wechat_appid'),
+            'secret' => env('wechat_key'),
             // 指定 API 调用返回结果的类型：array(default)/collection/object/raw/自定义类名
             'response_type' => 'array',
             //...
